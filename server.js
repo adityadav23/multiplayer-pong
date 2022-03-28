0dd1c2710a90177f701a531c84339a052ecc0ddf
Aditya Yadav <adityadav023>
2022-03-28 14:38:11 +0530

    Listening for events in pong  server

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ const io = require('socket.io')(server, {
 })
 const PORT = 3000
 
-
-
 server.listen(3000,()=>{
     console.log(`Listening on ${PORT} ...`)
 })
 
+let readyPlayerCount =0 ;
+
+
+
 io.on('connection',(socket)=>{
     console.log('A user is connected', socket.id)
+
+    socket.on('ready', ()=>{
+        console.log('Player ready ', socket.id)
+
+        readyPlayerCount++;
+
+        if(readyPlayerCount === 2){
+            // broadcast('startGame')
+        }
+    })
 })
