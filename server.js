b1a92580c1e31297a778bd0b6b73391dda9a8016
Aditya Yadav <adityadav023>
2022-03-28 14:22:17 +0530

    Implemented socket.io server and connected the user

---
+++ server.js
@@ -0,0 +1,17 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+    cors:{
+        origin: "*"
+    }
+})
+const PORT = 3000
+
+
+
+server.listen(3000,()=>{
+    console.log(`Listening on ${PORT} ...`)
+})
+
+io.on('connection',(socket)=>{
+    console.log('A user is connected')
+})
